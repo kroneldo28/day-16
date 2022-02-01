35d4ac3f87b4f09d18d692da05c37a07b5bbfc77
kroneldo28 <kroneldo28@yahoo.fr>
2022-02-01 22:53:57 +0100

    Playing with PrettyTable

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 table.align = "l"
-
+print(table)
+table.align = "r"
 print(table)
 
